6951d3f206a7b32f4887fff7be9ef11238063b23
Kingsley <kingsleyumujeyan@gmail.com>
2020-04-09 21:25:41 +0100

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
 const covid19ImpactEstimator = (data) => data;
 
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+const estimates = () => {
+  const currentlyInfected = data.reportedCases * 10;
+  const infectionsByRequestedTime = currentlyInfected * 512;
+  const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
+  const output = {
+    data: {},
+    impact: { currentlyInfected, infectionsByRequestedTime, severeCasesByRequestedTime },
+    severeImpact: {}
+  };
+  return output;
+};
+estimates();
 export default covid19ImpactEstimator;
