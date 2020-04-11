41e966fef9af350dc9d42652a5068640ad9167f2
Kingsley <kingsleyumujeyan@gmail.com>
2020-04-11 23:58:01 +0100

    CH1

--- src/estimator.js
+++ src/estimator.js
@@ -15,35 +15,37 @@ const input = {
   population: 92931687,
   totalHospitalBeds: 678874
 };
-const period = () => {
-  const { timeToElapse } = input;
-  if (input.periodType === 'months') return timeToElapse * 30;
-  if (input.periodType === 'weeks') return timeToElapse * 7;
-  if (input.periodType === 'days') return timeToElapse;
-  return input;
-};
-period();
-
 const factor = () => {
-  const x = input.timeToElapse / 3;
-  const y = (2 ** Math.trunc(x));
+  const x = (Math.trunc(input.timeToElapse / 3));
+  const y = (2 ** x);
   return y;
 };
 factor();
 
-const beds = () => {
+const period = () => {
+  if (input.periodType === 'months') return input.timeToElapse * 30;
+  if (input.periodType === 'weeks') return input.timeToElapse * 7;
+  if (input.periodType === 'days') return input.timeToElapse * 1;
+  return 0;
+};
+period();
+
+/* const beds = () => {
   const a = input.totalHospitalBeds * 0.35;
   const b = (Math.trunc(a));
   return b;
 };
-beds();
+beds(); */
 
 const impactEstimates = () => {
+  const multiplier = factor();
   const impactCurrentlyInfected = input.reportedCases * 10;
-  const impactInfectionsByRequestedTime = impactCurrentlyInfected * factor();
+  const impactInfectionsByRequestedTime = (impactCurrentlyInfected * multiplier);
   const severeCurrentlyInfected = input.reportedCases * 50;
-  const severeInfectionsByRequestedTime = severeCurrentlyInfected * factor();
+  const severeInfectionsByRequestedTime = (severeCurrentlyInfected * multiplier);
   const output = {
+    periodType: input.periodType,
+    timeToElapse: period(),
     estimates: {
       impact: {
         currentlyInfected: impactCurrentlyInfected,
